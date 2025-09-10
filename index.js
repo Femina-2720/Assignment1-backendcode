f2028a27fe474e6e518f965c240db9d1c857e8b0
Femi2720 <femi65669@gmail.com>
2025-09-10 19:37:49 +0530

    set back url

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://assignment1-frontendcode.onrender.com/cart",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use('/items', itemsRoute);
 app.use('/cart', cartRoutes);
